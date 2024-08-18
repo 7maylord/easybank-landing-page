542e3c494c38927da9478464002f379622f18ef2
Olumide Adenigba <Olumideadenigba@gmail.com>
2024-08-18 19:32:22 +0100

    final check

--- index.js
+++ index.js
@@ -4,11 +4,3 @@ const menuButton = document.getElementById("menu-button")
 menuButton.addEventListener("click", () => {
   navbar.classList.toggle("open")
 })
-// const navbar = document.querySelector("nav");
-// const menuButton = document.getElementById("menu-button");
-
-// if (navbar && menuButton) {
-//     menuButton.addEventListener("click", () => {
-//         navbar.classList.toggle("open");
-//     });
-// }
